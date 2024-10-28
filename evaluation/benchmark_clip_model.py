d000dbeae56c868f5e979745e29b7cf08ac0c9f3
AskSid <siddboppana@gmail.com>
2024-10-28 14:34:26 -0400

    modified benchmark code

--- evaluation/benchmark_clip_model.py
+++ evaluation/benchmark_clip_model.py
@@ -4,23 +4,25 @@ from unibench import Evaluator
 from unibench.models_zoo.wrappers.clip import ClipModel
 import open_clip
 import torch
+from transformers import GPT2Tokenizer
 
 import sys
 import os
 sys.path.append(os.path.abspath("../training"))
 import CustomCLIP
 
-
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 custom_clip = CustomCLIP(
-            "openai-community/gpt2", 
+            "openai-community/gpt2",
             "google/vit-huge-patch14-224-in21k", 
             embedding_dim=1024,
             use_peft=True
         ).to(device)
 
-tokenizer = open_clip.get_tokenizer("ViTamin-L")
+# need to load weights
+
+tokenizer = GPT2Tokenizer.from_pretrained("openai-community/gpt2")
 
 model = partial(
     ClipModel,
@@ -31,10 +33,11 @@ model = partial(
     logit_scale=custom_clip.logit_scale,
 )
 
+# might need to have datasets locally
 
-eval = Evaluator(benchmarks_dir="../benchmark_data/insert_benchmark_folder_here")
+eval = Evaluator() # benchmarks_dir="../benchmark_data/insert_benchmark_folder_here")
 
 eval.add_model(model=model)
-eval.update_benchmark_list(["benchmark_name"])
+eval.update_benchmark_list(["cifar10"])
 eval.update_model_list(["clip-small-text-large-vision"])
 eval.evaluate()
